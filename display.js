a1c6aa6dbf23c9dcad7e66137d3bab7f54868d7b
Zero Wu <zerowuchina@gmail.com>
2016-03-30 03:20:01 +0800

    add display.js

---
+++ display.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         灵梦御所
+// @namespace    https://github.com/zerowuchina
+// @version      0.1
+// @description  老司机钥匙
+// @author       Zero Wu
+// @match        https://blog.reimu.net/archives/*
+// @grant        none
+// @run-at      document-end
+// ==/UserScript==
+  var pre = document.getElementsByTagName('pre') [0];
+  pre.style.display = 'block';
